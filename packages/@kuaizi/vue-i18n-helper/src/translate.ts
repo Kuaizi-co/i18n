e648eb2563fddca6e9b30fefeeb8b61f8ab59760
tomieric <tomieric@gmail.com>
2019-09-06 18:48:06 +0800

    fix(vue-i18n-helper): keep old text selection

--- packages/@kuaizi/vue-i18n-helper/src/translate.ts
+++ packages/@kuaizi/vue-i18n-helper/src/translate.ts
@@ -34,29 +34,46 @@ class Translate {
   public into (): any {
     const editor = this._getEditor();
     const selections = editor.selections
-    let text: String;
+    const newSelections: vscode.Selection[] = [];
+    let text: string;
+    let replaceText: string;
 
     if (!editor) return this.showInfoMessage('Unselected text!');
 
     editor.edit((editBuilder: any) => {
       selections.forEach((selection: vscode.Selection) => {
+        const start: vscode.Position = selection.start;
+        const end: vscode.Position = selection.end;
         // Get SingleLine Text
         text = this.getWords(selection);
         // Replace text
-        pattern.find((p: any) => {
+        pattern.forEach((p: any) => {
           const result = p.pattern.test(text);
           if (result) {
-            text = text.replace(p.pattern, p.replacement);
+            replaceText = text.replace(p.pattern, p.replacement);
           }
-          return result
         });
         // Replace editor text
         editBuilder.replace(
-          new vscode.Range(selection.start, selection.end),
-          text
+          new vscode.Range(start, end),
+          replaceText
+        );
+
+        // Set new selection position
+        const index = replaceText.indexOf(text);
+        newSelections.push(
+          new vscode.Selection(
+            start.line,
+            start.character + index,
+            end.line,
+            start.character + index + text.length
+          )
         );
       })
     });
+
+    // Reset to select old text
+    editor.selections = newSelections;
   }
 }
 
